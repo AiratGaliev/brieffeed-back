084c907bc1f22f7c1f9613ab35be6b0fe6cd169b
Airat Galiev <airat.galiev@gmail.com>
2019-09-26 16:35:13 +0300

    start project

---
+++ src/main/java/com/brieffeed/backend/BrieffeedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brieffeed.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrieffeedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrieffeedBackendApplication.class, args);
+	}
+
+}
