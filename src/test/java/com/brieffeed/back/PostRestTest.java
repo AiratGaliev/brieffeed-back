6036fa3449577f2370394a9d14a521a70f9ef75b
Airat Galiev <airat.gaiev@gmail.com>
2021-03-31 19:43:46 +0300

    fixed authentication tests

--- src/test/java/com/brieffeed/back/PostRestTest.java
+++ src/test/java/com/brieffeed/back/PostRestTest.java
@@ -1,16 +1,17 @@
 package com.brieffeed.back;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -20,19 +21,31 @@ public class PostRestTest {
   @Autowired
   private MockMvc mockMvc;
 
-  // Тестирование айтентификации с правильными учетными данными
+  // Тестирование аутентификации с правильными учетными данными
   @Test
   public void testAuthenticationPositive() throws Exception {
-    this.mockMvc.perform(post("/login").content("{\"username\":\"test\", \"password\":\"test\"}"))
+    this.mockMvc.perform(
+        post("/api/users/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\n"
+                + "    \"username\": \"user\",\n"
+                + "    \"password\": \"user_password\"\n"
+                + "}"))
         .andDo(print())
         .andExpect(status().isOk());
   }
 
-  // Тестирование айтентификации с неверными учетными данными
+  // Тестирование аутентификации с неверными учетными данными
   @Test
   public void testAuthenticationNegative() throws Exception {
     this.mockMvc
-        .perform(post("/login").content("{\"username\":\"admin\", \"password\":\"wrongpwd\"}"))
+        .perform(
+            post("/api/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n"
+                    + "    \"username\": \"admin\",\n"
+                    + "    \"password\": \"wrongpwd\"\n"
+                    + "}"))
         .andDo(print()).andExpect(status().is4xxClientError());
   }
 
